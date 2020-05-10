b3364804e9fa633a62a418f6cb7f90d146a3d5ec
Gregory Bevan <gregory.bevan@zenika.com>
2020-05-10 22:28:44 +0200

    :sparkles: Add main to Left rotation

--- src/main/kotlin/com/hackerrank/arrays/LeftRotation.kt
+++ src/main/kotlin/com/hackerrank/arrays/LeftRotation.kt
@@ -1,7 +1,23 @@
 package com.hackerrank.arrays
 
+import java.util.*
+
 fun rotLeft(a: Array<Int>, d: Int): Array<Int> {
     val toMutableList = a.filterIndexed{ index, _ -> index >= d }.toMutableList()
     toMutableList.addAll(a.take(d))
     return toMutableList.toTypedArray()
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val nd = scan.nextLine().split(" ")
+
+    val n = nd[0].trim().toInt()
+
+    val d = nd[1].trim().toInt()
+
+    val a = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    println(rotLeft(a, d).joinToString(" "))
 }
